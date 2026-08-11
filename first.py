11d525a4c98f465ce22eb25c860e4f5b34fbc015
Estiak Ayon <estiakayon@gmail.com>
2026-08-11 13:48:31 +0600

    1- Class & Object

---
+++ first.py
@@ -0,0 +1,10 @@
+class employee:
+    def __init__(self):
+        self.id = 123
+        self.salary = 50000
+        self.designation = "SD"
+
+
+# object creation
+sam = employee()
+print(sam.id)

9e7a3601d81648f9cb937de062f09f74fa7e09df
Estiak Ayon <estiakayon@gmail.com>
2026-08-19 23:15:09 +0600

    End of Inheritance

--- inheritance.py
+++ inheritance.py
@@ -27,26 +27,26 @@
 # Super Keyword
 
 # Base class
-class Animal:
-    def __init__(self):
-        self.name = "Buddy"
+# class Animal:
+#     def __init__(self):
+#         self.name = "Buddy"
 
-    def speak(self):
-        print(f"{self.name} makes a sound.")
+#     def speak(self):
+#         print(f"{self.name} makes a sound.")
 
-# # Derived class
-class Dog(Animal):
-    def __init__(self, breed):
-        super().__init__()
-        self.breed = breed
+# # # Derived class
+# class Dog(Animal):
+#     def __init__(self, breed):
+#         super().__init__()
+#         self.breed = breed
 
-    def speak(self):
-        super().speak()  # Call the base class method
-        print(f"{self.name} barks. It is a {self.breed}.")
+#     def speak(self):
+#         super().speak()  # Call the base class method
+#         print(f"{self.name} barks. It is a {self.breed}.")
 
-# # Create an instance of Dog
-dog = Dog("Golden Retriever")
-dog.speak()
+# # # Create an instance of Dog
+# dog = Dog("Golden Retriever")
+# dog.speak()
 # Output:
 # Buddy makes a sound.
 # Buddy barks. It is a Golden Retriever.
